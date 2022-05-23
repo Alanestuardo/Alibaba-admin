e8724b1bca905c5f899cc8a166f4d4fd5f3f47b8
Alanestuardo <87455001+Alanestuardo@users.noreply.github.com>
2022-05-23 16:17:49 -0600

    Create Main.js

---
+++ js/Main.js
@@ -0,0 +1,20 @@
+/**********************
+ * INICIO DE SESIÓN
+ *********************/
+
+/* Librería de los programas */
+import {revisarusuario} from "./login.js"
+
+function recuperar_datos(e) {
+    e.preventDefault(); //<-- Evita que el formulario se recargue
+
+    var username = document.querySelector("#username").value;
+    var password = document.querySelector("#password").value;
+
+    //Programa login
+    revisarusuario(username,password);
+}
+
+// Acción del boton
+var bt_entrar = document.querySelector("#login");//<-- Instancia del formulario
+bt_entrar.addEventListener('submit',recuperar_datos); //<-- Evento
